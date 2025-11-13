93725ab04e066f90803d187656cd9a5c8e0c89d3
mayurakkar <mayurakkar10@gmail.com>
2025-11-13 19:26:01 +0530

    add CI pipeline configuration and update index.js response message

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World! this is the first version of my project.');
+  res.send('Hello World! this is the first version of my project. fasdfkjadfjlkfjdsafjaslfjsafl');
 });
 
 app.listen(port, () => {
