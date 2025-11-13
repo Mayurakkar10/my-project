9de1f335644dbe6e490b885438a09e4a9d780573
mayurakkar <mayurakkar10@gmail.com>
2025-11-13 11:33:53 +0530

    new changes

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-import express from 'express';
+const express = require('express');
 const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send('Hello World! this is the first version of my project.');
 });
 
 app.listen(port, () => {
